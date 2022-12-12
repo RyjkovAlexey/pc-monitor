606ec2b96f2bd5ec41dafe7e2d9159b4da30d022
RyjkovAlexey <kawai4ik98@gmail.com>
2022-12-12 22:36:20 +0300

    json export

--- silence_mode_app.py
+++ silence_mode_app.py
@@ -1,5 +1,7 @@
 import json
+import pathlib
 from datetime import datetime
+from typing import Dict, Any
 
 from base_plugin import BasePlugin
 from loader import PluginLoader
@@ -7,18 +9,25 @@ from plugin_report import PluginReport
 
 
 class SilenceModeApp:
-    def __init__(self, export_path: str, plugins: [PluginLoader]):
+    def __init__(self, export_path: str, _loaders: list[PluginLoader]):
         self._export_path = export_path
-        self._plugins: [PluginLoader] = plugins
+        self._loaders: list[PluginLoader] = _loaders
 
     def run(self):
-        results: [PluginReport] = []
-        for plugin in self._plugins:
-            info: dict[BasePlugin.BaseCharacteristic, str] = plugin.plugin.collect_information()
-            converted = {}
-            for result_row in info:
-                converted[result_row.feature_name] = info[result_row]
-            results.append(PluginReport(plugin.plugin.name, converted).__dict__)
+        res = list(map(self._get_results, self._loaders))
+        self._write_result_to_file(res)
 
-        with open('sample.json', 'w', encoding='utf-8') as outfile:
-            outfile.write(json.dumps(results, ensure_ascii=False))
+    @staticmethod
+    def _get_results(loader: PluginLoader) -> dict[str, Any]:
+        collected_info = loader.plugin.collect_information()
+        aggregate_results: Dict[str, str] = {}
+        for key in collected_info.keys():
+            aggregate_results[key.feature_name] = collected_info[key]
+
+        return PluginReport(loader.plugin_name, aggregate_results).__dict__
+
+    def _write_result_to_file(self, res: list[dict[str, Any]]):
+        path = pathlib.Path(f'{self._export_path}/{datetime.now().microsecond}.json')
+        print(path)
+        with open(path, 'w', encoding='utf-8') as outfile:
+            outfile.write(json.dumps(res, ensure_ascii=False))
